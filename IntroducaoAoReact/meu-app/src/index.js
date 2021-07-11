8478e870d72813d77175340646f1bb32290eabef
MathSena <MathSena07@hotmail.com>
2021-07-10 23:05:05 -0300

    React - Componentes Tipo Classe

---
+++ IntroducaoAoReact/meu-app/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import App2 from './components/App2';
+import App3 from './components/App3';
+import App4 from './components/App4';
+import App5 from './components/App5';
+import Lista from './components/Lista';
+import Formulario from './components/Formulario';
+
+
+
+
+
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+      <Formulario>
+
+      </Formulario>
+
+
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
